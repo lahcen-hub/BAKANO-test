9a4b4bf1542da531b7ce109b79e7ab7eb93f3af5
Lahcen Ait el kaid <Yokaitelkaid@gmail.com>
2025-08-31 22:06:16 +0000

    mett le nombre de total des eleve en dernirer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -398,22 +398,6 @@ export default function Home() {
             <Card>
               <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
                 <CardTitle className="text-sm font-medium">
-                  Total des élèves
-                </CardTitle>
-                <Users className="h-4 w-4 text-muted-foreground" />
-              </CardHeader>
-              <CardContent>
-                <div className="text-xl md:text-2xl font-bold">
-                  {totalStudentsCount}
-                </div>
-                <p className="text-xs text-muted-foreground">
-                  Inscrits dans tous les groupes
-                </p>
-              </CardContent>
-            </Card>
-            <Card>
-              <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                <CardTitle className="text-sm font-medium">
                   Total des revenus
                 </CardTitle>
                 <BadgeCent className="h-4 w-4 text-muted-foreground" />
@@ -475,6 +459,22 @@ export default function Home() {
                 </Button>
                 <p className="text-xs text-muted-foreground mt-1">
                   Analyse du mois dernier.
+                </p>
+              </CardContent>
+            </Card>
+            <Card>
+              <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                <CardTitle className="text-sm font-medium">
+                  Total des élèves
+                </CardTitle>
+                <Users className="h-4 w-4 text-muted-foreground" />
+              </CardHeader>
+              <CardContent>
+                <div className="text-xl md:text-2xl font-bold">
+                  {totalStudentsCount}
+                </div>
+                <p className="text-xs text-muted-foreground">
+                  Inscrits dans tous les groupes
                 </p>
               </CardContent>
             </Card>
